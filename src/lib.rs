b226ff10b1eea7187d13293f5e75789d87452c46
evilbocchi <71329833+evilbocchi@users.noreply.github.com>
2026-09-19 16:17:13 +0800

    refactor: move primitives to src/primitive

--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,24 @@
 //! A small, data-oriented 3D library built around `wgpu`.
 #![deny(missing_docs)]
 
-mod color3;
 mod engine;
-mod face;
 mod instance;
 mod lighting;
 mod material;
+mod primitive;
 mod renderer;
-mod shape;
 mod skybox;
 mod tween;
 mod vertex;
 
-pub use color3::*;
 pub use eframe::{self, egui, egui_wgpu, wgpu};
 pub use engine::*;
-pub use face::*;
 pub use glam;
 pub use instance::*;
 pub use lighting::*;
 pub use material::*;
+pub use primitive::*;
 pub use renderer::*;
-pub use shape::*;
 pub use skybox::*;
 pub use tween::*;
 pub use vertex::*;
---
+++ src/primitive/mod.rs
@@ -0,0 +1,7 @@
+mod color3;
+mod face;
+mod shape;
+
+pub use color3::*;
+pub use face::*;
+pub use shape::*;
